bd9034cfce31daf4e1c84c14189cec4a75a4cda3
Wasim A Pinjari <wasimapinjari@gmail.com>
2024-07-31 23:47:55 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,6 @@
 import { ReactNode } from "react";
 import Main from "./_components/Main";
 
-export default function Home({ children }: Readonly<{ children: ReactNode }>) {
+export default function Home({ children }: { children: ReactNode }) {
   return <Main />;
 }

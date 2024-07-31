4647e7577c5820631395166037ee920c873ba77b
Wasim A Pinjari <wasimapinjari@gmail.com>
2024-08-01 00:10:18 +0530

    first commit

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,11 +1,15 @@
 "use client";
 
+export async function generateStaticParams() {
+  return [];
+}
+
 import { useAppSelector } from "@/redux/hooks";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import Form from "../_components/Form";
 
-export default function FormWithId({ params }: { params: { id: string } }) {
+export default function CustomerForm({ params }: { params: { id: string } }) {
   const router = useRouter();
   const customer = useAppSelector((state) =>
     state.customers.find((customer) => customer.id === params.id),

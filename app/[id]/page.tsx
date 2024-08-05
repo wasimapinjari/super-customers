f3bbaf0b29c9792adbfc15a3f26998d8bcc794a0
Wasim A Pinjari <wasimapinjari@gmail.com>
2024-08-05 20:58:30 +0530

    fix

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,3 +1,8 @@
+export async function generateStaticParams() {
+  return store.getState().customers.map((customer) => ({ id: customer.id }));
+}
+
+import store from "@/redux/store";
 import FormWithID from "../_components/FormWithID";
 
 export default function CustomerForm({ params }: { params: { id: string } }) {

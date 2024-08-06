e1fe7833be810192cef0f2a80a6eb163dbc70411
Wasim A Pinjari <wasimapinjari@gmail.com>
2024-08-06 09:34:08 +0530

    fix

--- redux/Provider.tsx
+++ redux/Provider.tsx
@@ -1,11 +1,18 @@
 "use client";
 
-import store from "@/redux/store";
+import store, { persistor } from "@/redux/store";
 import { ReactNode } from "react";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 export default function ReduxProvider({
   children,
 }: Readonly<{ children: ReactNode }>) {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        {children}
+      </PersistGate>
+    </Provider>
+  );
 }
--- redux/store.ts
+++ redux/store.ts
@@ -1,12 +1,33 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import customerSlice from "./customerSlice";
 import formSlice from "./formSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
+import persistStore from "redux-persist/lib/persistStore";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const rootReducer = combineReducers({
+  customers: customerSlice,
+  form: formSlice,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: { customers: customerSlice, form: formSlice },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
 export default store;
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
